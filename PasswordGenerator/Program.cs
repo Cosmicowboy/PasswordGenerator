34cc19fbc29854083d57755667fce12f564ecfda
B-Morningstar <bmorningstar@ttool.net>
2026-03-18 09:24:30 -0500

    Add project files.

---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.CommandLine;
+
+namespace PasswordGenerator;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        
+    }
+}
+
+/*
+ * 
+ * command line password bank
+ * create (either insert or generate new)
+ *      -option to save generated password
+ * read (give site --> get password)
+ * update (give site --> new password)
+ * delete (give site --> delete)
+ */
