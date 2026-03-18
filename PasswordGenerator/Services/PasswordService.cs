ac1ee2f13f609010aeef7950b3dd53fa6f8da60e
B-Morningstar <bmorningstar@ttool.net>
2026-03-18 15:22:16 -0500

    Feature: built password generator

--- PasswordGenerator/Services/PasswordService.cs
+++ PasswordGenerator/Services/PasswordService.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Text;
 
 namespace PasswordGenerator.Services;
 
@@ -10,13 +8,36 @@ public class PasswordService
     private const string _upperPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     private const string _numberPool = "0123456789";
     private const string _specialPool = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+    private readonly List<string> _stringPools = [_lowerPool, _upperPool, _numberPool, _specialPool];
+    private int _lastPoolSelection;
     public string GeneratePassword(ushort minLength)
     {
-        //each pool gets a num 
-        //each iteration draw from a pool and record number
-        // each draw is also a random num
+
         var sBuilder = new StringBuilder();
-        return password;
+        var rnd = new Random();
+
+        var poolNum = rnd.Next(_stringPools.Count);
+
+        for (int i = 0; i < minLength; i++)
+        {
+            //random number to get the pool
+
+            while (poolNum == _lastPoolSelection)
+            {
+                poolNum = rnd.Next(_stringPools.Count);
+            }
+            _lastPoolSelection = poolNum;
+
+            var drawingPool = _stringPools[poolNum];
+
+            var currentChar = drawingPool[rnd.Next(drawingPool.Length)];
+
+            sBuilder.Append(currentChar);
+
+        }
+        // each draw is also a random num
+
+        return sBuilder.ToString();
     }
     //serialize passwords
     //deserialize passwords
